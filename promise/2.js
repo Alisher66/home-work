ba2746a935eb6f21c34b761dcda17b75bd83ad09
Alisher <alisher1929@umail.uz>
2021-12-17 15:39:12 +0300

    promise 4

--- promise/2.js
+++ promise/2.js
@@ -4,6 +4,57 @@ const functionArr = [
     () => new Promise((res) => setTimeout(() => res("programming language"), 450)),
 ];
 
+
+
+// 1.  ===========================
+
+function handler(arr) {
+    let str = "";
+    return new Promise((res) => {
+        arr.forEach((fn, index) => {
+            fn().then(arg => {
+                str += arg+ " ";
+                if (index == arr.length - 1) {
+                    res(str.trim());
+                }
+            })
+        })
+    })
+}
+
+handler(functionArr).then((aaa) => {
+    console.log(aaa);
+});
+
+
+// 2.  ===========================
+
+// function heandle(arr) {
+//     let temp = [];
+//     arr.forEach(el => {
+//         el().then((str) => {
+//             temp.push(str);
+//         });
+//     });
+//     return new Promise((res) => {
+//         let interval = setInterval(() =>{
+//             if(temp.length == arr.length){
+//                 clearInterval(interval);
+//                 res(temp.join(" "));
+//             }
+//         },0);
+//     });
+// }
+// heandle(functionArr).then((res) =>{
+//     console.log(res);
+// })
+
+
+
+
+
+// 3.  ===========================
+
 // const handler = (arr) => {
 //     return Promise.all( arr.map(el => el()) )
 //                     .then(val=> val.join(" "));
@@ -11,8 +62,12 @@ const functionArr = [
 // };
 
 // handler(functionArr).then((res) => {
-//     console.log(res); 
+//     console.log(res);
 // });
+
+
+
+// 4.  ===========================
 
 // const heandle = (arr, finalStr = "") => {
 //     return !arr.length ? finalStr.trim() : ([curr, ...rest] = arr)
@@ -20,30 +75,14 @@ const functionArr = [
 // }
 
 
-
 // heandle(functionArr).then((res) => {
 //     console.log(res);
 // })
 
 
-function heandle(arr) {
-    let temp = [];
-    arr.forEach(el => {
-        el().then((str) => {
-            temp.push(str);
-        });
-    });
-    return new Promise((res) => {
-        let interval = setInterval(() =>{
-            if(temp.length == arr.length){
-                clearInterval(interval);
-                res(temp.join(" "));
-            }
-        },0);
-    });
-}
-heandle(functionArr).then((res) =>{
-    console.log(res);
-})
+
+
+
+
 
 
